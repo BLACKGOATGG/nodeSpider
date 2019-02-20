a03018a63605810f944cc58d824aadfc49ad3561
hzgjl <hzgjl@tairanchina.com>
2019-02-20 15:09:09 +0800

    清理

--- 项目描述.js
+++ 项目描述.js
@@ -1,14 +1,17 @@
 //node爬虫系列
 
 const dependencies = {
-  "async": "^2.6.1", //控制并发
-  "cheerio": "^1.0.0-rc.2", //jQuery核心实现
-  "express": "^4.16.3", //模块化
-  "fs": "^0.0.1-security", //文件系统
-  "iconv-lite": "^0.4.23", //转换字符编码
-  "request": "^2.87.0", //请求
-  "superagent": "^3.8.3", //超级代理
-  "superagent-charset": "^1.2.0" //superagent的转码
+  "fs": "^0.0.1-security",        //文件系统
+  "express": "^4.16.3",           //模块化
+
+  "request": "^2.87.0",           //请求
+  "iconv-lite": "^0.4.23",        //转换字符编码
+  "superagent": "^3.8.3",         //超级代理
+  "superagent-charset": "^1.2.0", //superagent的转码
+
+  "async": "^2.6.1",              //控制并发
+  "cheerio": "^1.0.0-rc.2",       //jQuery核心实现
+  
 }
 
 /* 
